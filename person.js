24f7da2f44794d7ef8a825903a6461a82a0786f8
Mahi Jodhani <164493944+mahijodhani@users.noreply.github.com>
2024-10-16 14:31:11 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    getDetails() {
+        return `Name: ${this.name}`;
+    }
+
+ 
+    static greet = () => "Hello, welcome to our T-shirt store!";
+}
+
